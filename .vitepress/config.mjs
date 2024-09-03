30381fc2f913b60a2eda44d53f4dbe517ffb25a7
Amino Acid <Siying.Han0208@Gmail.com>
2024-09-04 00:51:51 +0800

    update config

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/vuepress-starter/',
   title: "ChatParts",
   description: "Procedure of using Chat-Parts",
   themeConfig: {
